9411671794807a59cbd56cb83481622676adcfc5
agusberal <laumaldo006@gmail.com>
2024-11-08 16:04:14 -0300

    cambios

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+
+#--->Traemos la Tablas desde MODELS
+from .models import *
+
+#---->traemos los formularios generales
+from .forms import *
+
+
+
 # Register your models here.
+admin.site.register(NuevoVehiculo)
+
